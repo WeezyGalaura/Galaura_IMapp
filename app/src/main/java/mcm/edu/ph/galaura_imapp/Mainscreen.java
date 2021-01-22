341d2aa8b812626c861ee1183e6304a43d543e2a
WeezyGalaura <wmgalaura@mcm.edu.ph>
2021-01-22 10:47:45 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/galaura_imapp/Mainscreen.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.galaura_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
